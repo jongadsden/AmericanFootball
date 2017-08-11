b1c5cfeded1d859f0e34e1812ad82a36d4a0b03c
Jonathan Gadsden <jonathangadsden@jonathans-air>
2017-08-11 20:46:27 +0100

    update

--- AmericanFootball/mainViewController.swift
+++ AmericanFootball/mainViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 var homeScore = 0
 var awayScore = 0
 var teamInPossession = 1
+var currentQuarter = 1
+var timeRemaining = "15:00"
+var currentDown = 1
+var yardsToGo = 10
+var ballHalf = 1
+var ballOn = 25
+var playTime = 45
+var yardsGained = 0
 
 class MainViewController: UIViewController {
 
@@ -24,6 +32,13 @@ class MainViewController: UIViewController {
     @IBOutlet weak var homePossession: UIImageView!
     @IBOutlet weak var awayScoreLabel: UILabel!
     @IBOutlet weak var awayPossession: UIImageView!
+    @IBOutlet weak var quarter: UILabel!
+    @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet weak var downLabel: UILabel!
+    @IBOutlet weak var toGoLabel: UILabel!
+    @IBOutlet weak var ballHalfLabel: UILabel!
+    @IBOutlet weak var ballOnLabel: UILabel!
+    @IBOutlet weak var playTimeLabel: UILabel!
     
     enum PlayType: Int {
         case pass = 0, run, kick
@@ -39,6 +54,13 @@ class MainViewController: UIViewController {
         teamInPossession = 1
         homePossession.isHidden = false
         awayPossession.isHidden = true
+        currentQuarter = 1
+        timeRemaining = "15:00"
+        currentDown = 1
+        yardsToGo = 10
+        ballHalf = 1
+        ballOn = 25
+        playTime = 45
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,21 +72,28 @@ class MainViewController: UIViewController {
        print("before score \(teamInPossession)")
         switch (PlayType(rawValue: sender.tag)!) {
         case .run:
-            score(ScoreType: "touchdown")
+            yardsGained = 2
         case .pass:
-            score(ScoreType: "touchdown")
+            yardsGained = 6
         case .kick:
             score(ScoreType: "fieldGoal")
         }
-        print ("after score \(teamInPossession)")
-        switchPossession()
-        print ("after switching \(teamInPossession)")
+        playCompletion()
         refreshScoreboardAfterPlay()
     }
     
     func refreshScoreboardAfterPlay() {
         homeScoreLabel.text = "\(homeScore)"
         awayScoreLabel.text = "\(awayScore)"
+        downLabel.text = "\(currentDown)"
+        toGoLabel.text = "\(yardsToGo)"
+        if ballHalf == 1 {
+            ballHalfLabel.text = "Home Team's"
+        } else {
+            ballHalfLabel.text = "Away Team's"
+        }
+        ballOnLabel.text = "\(ballOn)"
+        playTimeLabel.text = "45"
     }
     
     func score(ScoreType: String) {
@@ -75,6 +104,12 @@ class MainViewController: UIViewController {
                 score = 7
             case "fieldGoal":
                 score = 3
+            case "safety":
+                score = 2
+            case "twoPointConversion":
+                score = 2
+            case "extraPoint":
+                score = 1
             default:
                 score = 0
         }
@@ -84,6 +119,36 @@ class MainViewController: UIViewController {
         }
         else {
             awayScore += score
+        }
+    }
+    
+    func playCompletion() {
+        ballOn += yardsGained
+        yardsToGo -= yardsGained
+
+        if ballOn > 100 {
+            score(ScoreType: "touchdown")
+            // this is wrong and needs changing
+            switchPossession()
+        }
+        
+        if yardsToGo < 1 {
+            currentDown = 1
+            if ballOn > 89 {
+                yardsToGo = 100 - ballOn
+            }
+            else {
+                yardsToGo = 10
+            }
+        }
+        else {
+            currentDown += 1
+        }
+        
+        if currentDown >= 5 {
+            currentDown = 1
+            yardsToGo = 10
+            switchPossession()
         }
     }
     
